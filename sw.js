e841fc51185366ae94e9a31311cf3c96aeb748f7
Harinto José Rodríguez Cruz <djhjrc96@gmail.com>
2026-06-24 01:28:13 -0600

    warning

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const APP_CACHE = 'velocimetro-app-v7';
-const RUNTIME_CACHE = 'velocimetro-runtime-v7';
+const APP_CACHE = 'velocimetro-app-v8';
+const RUNTIME_CACHE = 'velocimetro-runtime-v8';
 const ASSETS = [
   './',
   './index.html',
