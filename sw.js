bec0e899ab89b7165d203216167caaab802d3f79
Harinto José Rodríguez Cruz <djhjrc96@gmail.com>
2026-06-24 01:13:44 -0600

    mejoras

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const APP_CACHE = 'velocimetro-app-v6';
-const RUNTIME_CACHE = 'velocimetro-runtime-v6';
+const APP_CACHE = 'velocimetro-app-v7';
+const RUNTIME_CACHE = 'velocimetro-runtime-v7';
 const ASSETS = [
   './',
   './index.html',
